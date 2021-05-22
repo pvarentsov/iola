53eca67e498348ef4c2a6e60755303a2932b40fc
pvarentsov <varentsovpavel@gmail.com>
2021-05-22 16:19:52 +0300

    Update main file

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,21 @@
 import prompts = require('prompts')
-import { Choice } from 'prompts'
 import { EnumUtil } from './core/common'
 import { SocketFactory, SocketType } from './core/socket'
 
 (async (): Promise<void> => {
   const socketTypes = EnumUtil.values(SocketType)
 
-  const choices: Choice[] = socketTypes.map(type => ({
-    title: type,
-    value: type,
-  }))
-
   const response = await prompts({
-    type: 'select',
-    choices: choices,
     name: 'type',
+    type: 'select',
     message: 'Select socket type',
-    validate: value => socketTypes.includes(value) ? true : `Available types: ${socketTypes}`
+    choices: socketTypes.map(type => ({
+      title: type,
+      value: type,
+    })),
+    validate: value => socketTypes.includes(value)
+      ? true
+      : `Available types: ${socketTypes}`
   })
 
   const client = SocketFactory.createClient(response.type)
