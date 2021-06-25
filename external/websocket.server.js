257232460abd7e2373b4e91d09e9df4af40e4ae9
pvarentsov <varentsovpavel@gmail.com>
2021-06-26 01:42:10 +0300

    Update demo

--- external/websocket.server.js
+++ external/websocket.server.js
@@ -6,11 +6,6 @@ const wss = new WebSocket.Server({ port: 8080 })
 wss.on('connection', ws => {
   ws.on('message', data => {
     const message = data.toString()
-
-    if (message === 'Something Ends') {
-      ws.send(Buffer.from('Something Begins'))
-    }
-
     console.log(message)
   })
 
