be09dade74cb6d2aee7acdc2cffad4ced9dba39e
pvarentsov <varentsovpavel@gmail.com>
2021-08-22 23:37:52 +0300

    Update cli version

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { SocketEventType, SocketFactory } from '@iola/core/socket'
 
 (async (): Promise<void> => {
   try {
-    const version = '0.3.1'
+    const version = '0.4.0'
 
     const config = CliFactory
       .createParser(version)
