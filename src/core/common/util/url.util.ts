34cb7acd1ecba35e7cc07a14a38297f6af1d2fe5
pvarentsov <varentsovpavel@gmail.com>
2022-07-21 01:03:55 +0300

    Fix sonar issues

--- src/core/common/util/url.util.ts
+++ src/core/common/util/url.util.ts
@@ -3,6 +3,8 @@ import { URL } from 'url'
 export class UrlUtil {
   static removeSearchParams(url: string): string {
     const parsed = new URL(url)
+
+    parsed.hash = ''
     parsed.search = ''
 
     return parsed.toString()
