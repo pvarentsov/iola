e0ed0cbbd83b8e6efa83b65d240504f980aaa655
pvarentsov <varentsovpavel@gmail.com>
2022-07-22 13:32:39 +0300

    Fix linter issues

--- src/api/http/nest/http.schema.ts
+++ src/api/http/nest/http.schema.ts
@@ -8,88 +8,88 @@ import { SocketEventType } from '@iola/core/socket'
 
 export class MessageModel {
   @ApiProperty({type: 'number'})
-  id: number
+    id: number
 
   @ApiProperty({enum: SocketEventType})
-  type: SocketEventType
+    type: SocketEventType
 
   @ApiProperty({type: 'string', example: new Date()})
-  date: Date
+    date: Date
 
   @ApiProperty({type: 'object'})
-  message: any
+    message: any
 }
 
 export class ErrorModel {
   @ApiProperty({type: 'number'})
-  statusCode: number
+    statusCode: number
 
   @ApiProperty({type: 'object'})
-  message: any
+    message: any
 
   @ApiProperty({type: 'string'})
-  error: string
+    error: string
 }
 
 export class GetMessageListQuery {
   @IsOptional()
   @IsIn(EnumUtil.values(SocketEventType), {each: true})
   @Transform(toArray)
-  type: SocketEventType[]
+    type: SocketEventType[]
 }
 
 export class SendDataBody {
   @ApiProperty({type: 'object', description: 'Any data'})
   @ValidateIf(object => object.bytes === undefined)
-  data: any
+    data: any
 
   @ApiProperty({type: 'number', isArray: true, description: 'UInt8 Array'})
   @ValidateIf(object => object.data === undefined)
   @IsArray()
   @Min(0, {each: true})
   @Max(255, {each: true})
-  bytes: number[]
+    bytes: number[]
 
   @ApiProperty({type: 'string', required: false, description: 'Only for SocketIO', default: '*'})
   @IsOptional()
   @IsString()
-  event?: string
+    event?: string
 }
 
 export class SendDataMessageBody {
   @ApiProperty({type: 'object', description: 'Any data'})
-  data: any
+    data: any
 
   @ApiProperty({type: 'string', required: false, description: 'Only for SocketIO', default: '*'})
-  event?: string
+    event?: string
 }
 
 export class SendBytesMessageBody {
   @ApiProperty({type: 'number', isArray: true, description: 'UInt8 Array'})
-  bytes: number[]
+    bytes: number[]
 
   @ApiProperty({type: 'string', required: false, description: 'Only for SocketIO', default: '*'})
-  event?: string
+    event?: string
 }
 
 export class SendMessageReply {
   @ApiProperty({enum: MessageFormat})
-  format: MessageFormat
+    format: MessageFormat
 
   @ApiProperty({type: 'object', description: 'Any data'})
-  data: any
+    data: any
 
   @ApiProperty({type: 'number', required: false, description: 'Only for binary messages'})
-  size?: number
+    size?: number
 
   @ApiProperty({type: 'string', required: false, description: 'Only for SocketIO'})
-  event?: string
+    event?: string
 }
 
 export class SendMessageResponse {
   @ApiProperty({type: 'number'})
-  messageId: number
+    messageId: number
 
   @ApiProperty({type: SendMessageReply, required: false})
-  reply?: SendMessageReply
+    reply?: SendMessageReply
 }
